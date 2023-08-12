e6398a950e73906844ca4e78749e018d133a7026
prasenjit52282 <prasenjitkarmakar52282@gmail.com>
2023-08-12 13:44:12 +0530

    restructured

---
+++ src/modules/__init__.py
@@ -0,0 +1,4 @@
+from .agent import Agent
+from .wall import Wall
+from .goal import Goal
+from .state import State
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from gridworld import GridWorld
+from src import GridWorld
 
 import numpy as np
 
